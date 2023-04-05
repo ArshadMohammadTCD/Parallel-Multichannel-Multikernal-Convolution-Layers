899b65a57646c2b6a60ad8d5d3bf1b8a5f9c50c5
ArshadMohammedTCD <mohammar@tcd.ie>
2023-04-05 13:15:29 +0100

    Failed vectorisation (Sad Day)

--- conv-harness.c
+++ conv-harness.c
@@ -336,7 +336,7 @@ void student_conv(float *** image, int16_t **** kernels, float *** output,
   //   }
   int h, w, x, y, c, m;
   
-  float image2[nchannels][width][height];
+  float image2[nchannels][width+kernel_order][height+kernel_order];
   // Transpose the matrix to image[c][w][h]
   for (w = 0; w < width; w++) {
         for (h = 0; h < height; h++) {
@@ -353,25 +353,30 @@ void student_conv(float *** image, int16_t **** kernels, float *** output,
         double sum = 0.0;
         for ( c = 0; c < nchannels; c++ ) {
           for (x=0; x< (kernel_order); x++){
-            for (y=0; y<(kernel_order); y+=4){            
+            for (y=0; y<(kernel_order-3); y+=4){            
+
                 // float imageArray[4] = {image[w+x][h+y][c], image[w+x][h+y][c+1], image[w+x][h+y][c+2], image[w+x][h+y][c+3]};
-                __m128 imageV = _mm_loadu_ps(&(image[c][w+x][h+y]));
+                __m128 imageV = _mm_loadu_ps(&(image2[c][w+x][h+y]));
               // Multiply vectors to format (image1*kernal1, image2*kernal2,...)
                 //float kernelVal1 = (float)(kernels[m][c][x][y]);
-                //float kernelVal2 = (float)(kernels[m][c][x][y+1]);
+               //float kernelVal2 = (float)(kernels[m][c][x][y+1]);
                 //float kernelVal3 = (float)(kernels[m][c][x][y+2]);
                 //float kernelVal4 = (float)(kernels[m][c][x][y+3]);
 
-                __m128i tempV = _mm_loadu_si128(&kernels[m][c][x][y]);
+                //__m128i tempV = _mm_loadu_si128(&kernels[m][c][x][y]);
                 __m128i tempV = _mm_loadu_si128((__m128i*)&kernels[m][c][x][y]);
                 __m128 kernelV = _mm_cvtepi32_ps(_mm_unpacklo_epi16(tempV, _mm_setzero_si128()));
 
                 //__m128 kernelV = _mm_set_ps(kernelVal1, kernelVal2, kernelVal3, kernelVal4);
+                
                 __m128 mulV = _mm_mul_ps(imageV, kernelV);
                 __m128 sumV = _mm_hadd_ps(mulV, mulV);
                 sumV = _mm_hadd_ps(sumV, sumV);
                 // sum of result
                 sum += _mm_cvtss_f32(sumV);
+            }
+            for (y; y < kernel_order; y++){
+                sum += image2[c][w+x][h+y]*kernels[m][c][x][y];
             }
           }
           output[m][w][h] = (float) sum; // output[kernal][width][height] = calculated sum
